55e679f38e0894163093504f6d49a6c3ad283d64
Sergii <skokopok@gmail.com>
2016-05-06 02:14:42 +0300

    ticket:1 model and migration has added

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,16 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField("First Name", max_length=30)
+    last_name = models.CharField("Last Name", max_length=30)
+    birth_date = models.DateField("Date of Birth")
+    con_email = models.EmailField("Email", max_length=79)
+    con_jabbber = models.EmailField("Jabber", max_length=79)
+    con_skype = models.CharField("Skype", max_length=79)
+    bio = models.TextField("Bio", max_length=400)
+    con_other = models.TextField("Other Contacts", max_length=400, blank=True)
+
+    def __str__(self):
+        return "%i  %s   %s" % (self.pk, self.first_name, self.last_name)
