df0411ca18181e496ec9feabe48527c669b9ffb7
Sergii <skokopok@gmail.com>
2016-05-06 02:24:40 +0300

    ticket:1 views and urls have configured

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# -*- encoding: utf-8 -*-
+from django.views.generic.base import TemplateView
+from apps.hello.models import Person
 
-# Create your views here.
+
+class MainPageView(TemplateView):
+
+    template_name = "hello/mainpage.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(MainPageView, self).get_context_data(**kwargs)
+        context['title'] = 'Hello'
+        context['object'] = Person.objects.first()
+
+        return context
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -5,9 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('hello.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
