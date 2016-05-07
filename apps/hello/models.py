a54e7b8558c10e7ac54e1fb8c584b575f281b89c
Sergii <skokopok@gmail.com>
2016-05-08 02:40:48 +0300

    ticket:3 model for request collecting has added + migration

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -10,7 +10,21 @@ class Person(models.Model):
     con_jabbber = models.EmailField("Jabber", max_length=79)
     con_skype = models.CharField("Skype", max_length=79)
     bio = models.TextField("Bio", max_length=400)
-    con_other = models.TextField("Other Contacts", max_length=400, blank=True)
+    con_other = models.TextField("Other Contacts",
+                                 max_length=400, blank=True)
 
     def __str__(self):
         return "%i  %s   %s" % (self.pk, self.first_name, self.last_name)
+
+
+class RequestCollect(models.Model):
+    r_method = models.CharField(max_length=20, blank=True)
+    r_path = models.CharField(max_length=50)
+    r_time = models.DateTimeField(auto_now_add=True)
+    r_status = models.CharField(max_length=5)
+    r_viewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        pretty_date = self.r_time.strftime('[%d-%m-%Y %I:%M:%S]')
+        return "%s\t%s\t'%s'\t%s" % (pretty_date, self.r_method,
+                                     self.r_path, self.r_status)
