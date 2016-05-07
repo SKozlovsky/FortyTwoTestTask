50b66aba853fbd553aa49dca91475576466863ea
Sergii <skokopok@gmail.com>
2016-05-08 02:19:58 +0300

    ticket:3 hardcoded page

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,16 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.views.generic.base import TemplateView
-from apps.hello.models import Person
 
 
 class MainPageView(TemplateView):
-
-    template_name = "hello/mainpage.html"
-
-    def get_context_data(self, **kwargs):
-
-        context = super(MainPageView, self).get_context_data(**kwargs)
-        context['title'] = 'Hello'
-        context['object'] = Person.objects.first()
-
-        return context
+    template_name = "hello/hc.html"
