85075306edb93abbc9cf17a8f7b4b54f50edbbc4
Sergii <skokopok@gmail.com>
2016-05-20 00:09:04 +0300

    ticket:5 views for the edit form and for the authentification have added

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
-from apps.hello.views import RequestsView, MainPageView, req_json
-from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+from apps.hello.views import RequestsView, MainPageView, req_json, \
+    PersonUpdate, ajax_login
 
 
 urlpatterns = [url(r'^$', MainPageView.as_view(), name='mainpage'),
                url(r'^requests$', RequestsView.as_view(), name='requests'),
                url(r'^req_json$', req_json, name='ajax_requests'),
-               url(r'^edit$', TemplateView.as_view(template_name="hello/hc_edit.html")),
+               url(r'^edit$', PersonUpdate.as_view(), name='edit'),
+               url(r'^ajax_login$', ajax_login, name='ajax_login'),
+               url(r'^login$', auth_views.login, {'template_name': 'hello/login.html'},
+                   name='login'),
+               url(r'^logout$', auth_views.logout, {'next_page': 'mainpage'},
+                   name='logout'),
                ]
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,9 +1,15 @@
-
 # -*- encoding: utf-8 -*-
 import json
 from django.views.generic.base import TemplateView
 from apps.hello.models import Person, RequestCollect
 from django.http import HttpResponse
+from django.core.context_processors import csrf
+from django.views.generic.edit import UpdateView
+from apps.hello.forms import LoginForm, EditForm
+from django.contrib import auth
+from django.shortcuts import render_to_response, HttpResponseRedirect
+
+import time
 
 
 class MainPageView(TemplateView):
@@ -48,3 +54,68 @@ def req_json(request):
     response = HttpResponse(json.dumps(response_data),
                             content_type="application/json")
     return response
+
+
+class PersonUpdate(UpdateView):
+    model = Person
+    template_name = "hello/edit.html"
+    form_class = EditForm
+    success_url = '/'
+
+    def get_object(self):
+        self.obj = Person.objects.first()
+        return self.obj
+
+    def get_context_data(self, **kwargs):
+        context = super(PersonUpdate, self).get_context_data(**kwargs)
+        context['title'] = 'edit'
+        context['object'] = self.obj
+        context['login_form'] = LoginForm
+        return context
+
+    def form_valid(self, form):
+        response = super(PersonUpdate, self).form_valid(form)
+
+        if self.request.is_ajax():
+            response_data = {'succses': True}
+            response_data["photo"] = "/static/uploads/%s?nocashe=%s" % \
+                                     (self.obj.photo, str(time.time()))
+            response = HttpResponse(json.dumps(response_data),
+                                    content_type="application/json")
+        return response
+
+    def form_invalid(self, form):
+        response = super(PersonUpdate, self).form_invalid(form)
+
+        if self.request.is_ajax():
+            # print dir(form)
+            if form.errors:
+                errors = form.errors
+                print errors
+                for (field, error) in form.errors.iteritems():
+                    print 'field ', field, 'error  ',  form.errors.get(field)
+                print dir(form.errors)
+                # print form.errors.iteritems()
+                # print type(form.errors.iteritems())
+            response_data = {'succses': False, 'errors': form.errors.__str__()}
+            response = HttpResponse(json.dumps(response_data),
+                                    content_type="application/json")
+        return response
+
+
+def ajax_login(request):
+    if request.is_ajax() and request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            response_data = {'succses': True, 'user': username}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+        else:
+            response_data = {'succses': False}
+            response_data['error'] = 'Username or Password incorrect'
+            return HttpResponse(json.dumps(response_data),
+                                content_type="application/json")
+
+    return HttpResponseRedirect('/login')
